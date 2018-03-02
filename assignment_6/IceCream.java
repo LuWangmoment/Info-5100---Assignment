c8a719f64b604f26847e9a6504089ac8d15f986d
LuWangmoment <35610738+LuWangmoment@users.noreply.github.com>
2018-03-01 20:26:14 -0800

    assignment_6

---
+++ assignment_6/IceCream.java
@@ -0,0 +1,23 @@
+package assignment666;
+
+public class IceCream extends DessertItem {
+	
+
+		protected int cost;
+
+		public IceCream(String _n, int _cost) {
+
+		super(_n);
+
+		cost = _cost;
+
+		}
+
+		public int getCost() {
+
+		return cost;
+
+		}
+
+		}
+
